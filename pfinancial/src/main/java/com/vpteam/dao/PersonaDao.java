5d3683b661a68e34a0060ad2a3e0d37c7c779356
quincho77 <ppasosv@hotmail.com>
2017-06-11 22:15:16 -0600

    CRUD completed

--- pfinancial/src/main/java/com/vpteam/dao/PersonaDao.java
+++ pfinancial/src/main/java/com/vpteam/dao/PersonaDao.java
@@ -6,26 +6,23 @@ import static com.vpteam.dao.Conexion.logger;
 import com.vpteam.entities.Persona;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonaDao 
 {
-    private Statement estado;
-    
     public void insertar(Persona objPersona)
-    {   
-        String query = "INSERT INTO personas (nombre, apellidos, sexo, cedula) VALUES " + 
-                       "(" + "'" + objPersona.getNombre() + "'" + 
-                       ", " + "'" + objPersona.getApellidos() + "'" + 
-                       ", " + + objPersona.getSexo() + 
-                       ", " + "'" + objPersona.getCedula() + "'" + ")";
-        
+    {           
         try 
-        {                    
-            estado = Conexion.obtenerInstancia().obtenerConexion().createStatement();  // EN ESTA LINEA ME DA EL ERROR
-            estado.execute(query);
+        {
+            PreparedStatement estado = Conexion.obtenerInstancia().obtenerConexion().prepareStatement(
+                    "INSERT INTO personas (nombre, apellidos, sexo, cedula) VALUES (?, ?, ?, ?)");
+            estado.setString(1, objPersona.getNombre());
+            estado.setString(2, objPersona.getApellidos());
+            estado.setInt(3, objPersona.getSexo());
+            estado.setString(4, objPersona.getCedula());
+            estado.execute();
         }
         catch(SQLException exception)
         {
@@ -41,12 +38,15 @@ public class PersonaDao
         
         try 
         {
-            estado = Conexion.obtenerInstancia().obtenerConexion().createStatement();
-            rs = estado.executeQuery("SELECT * FROM personas");
+             PreparedStatement estado = Conexion.obtenerInstancia().obtenerConexion().prepareStatement(
+                    "SELECT * FROM personas");
+            //estado = Conexion.obtenerInstancia().obtenerConexion().createStatement();
+            rs = estado.executeQuery();
             
             while(rs.next())
-                lista.add( new Persona( rs.getString(1), rs.getString(2), rs.getInt(3),
-                        rs.getString(4) ) );
+                lista.add( new Persona( rs.getString(2), rs.getString(3), rs.getInt(4),
+                        rs.getString(5) ) );
+            
         }
         catch(SQLException exception)
         {
@@ -55,5 +55,43 @@ public class PersonaDao
         }
         
         return lista;
+    }
+    
+    public void eliminar(int id)
+    {
+        try
+        {
+            PreparedStatement estado = Conexion.obtenerInstancia().obtenerConexion().prepareStatement(
+                    "DELETE * FROM personas WHERE id = ?");
+            estado.setInt(0, id);
+            estado.executeUpdate();
+        }
+        catch(SQLException exception)
+        {
+            logger.error(exception);
+            System.out.println("Error: " + exception.getErrorCode() + exception.getMessage());
+        }
+    }
+    
+    public void actualizar(int id, Persona persona)
+    { 
+        String query = "UPDATE personas SET nombre = ?, apellidos = ?, sexo = ?, cedula = ? " +
+                       "WHERE id = ?";
+        
+        try 
+        {
+            PreparedStatement estado = Conexion.obtenerInstancia().obtenerConexion().prepareStatement(query);
+            estado.setString(1, persona.getNombre());
+            estado.setString(2, persona.getApellidos());
+            estado.setInt(3, persona.getSexo());
+            estado.setString(4, persona.getCedula());
+            estado.setInt(5, id);
+            estado.executeUpdate();
+        }
+        catch(SQLException exception)
+        {
+            logger.error(exception);
+            System.out.println("Error: " + exception.getErrorCode() + exception.getMessage());
+        }
     }
 }
