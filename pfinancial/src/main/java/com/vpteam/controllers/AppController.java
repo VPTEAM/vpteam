be9ab6c83190a027cae6946d7b86565e574255a8
cooper15 <cooper15@debi>
2017-06-21 23:30:34 -0600

    La paginacion esta funcionando.

--- pfinancial/src/main/java/com/vpteam/controllers/AppController.java
+++ pfinancial/src/main/java/com/vpteam/controllers/AppController.java
@@ -1,5 +1,8 @@
 package com.vpteam.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +20,6 @@ import org.springframework.web.context.request.WebRequest;
 
 import com.vpteam.dao.PersonaDao;
 import com.vpteam.entities.Persona;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class AppController 
@@ -51,45 +52,45 @@ public class AppController
 	@RequestMapping(value="/dashboard")
 	public String dashboard(Model model)
 	{
-            model.addAttribute("persona", new Persona());
-            List<Persona> listaPersona = new ArrayList<>();
-            PersonaDao personaDao = new PersonaDao();
-            listaPersona = personaDao.seleccionar();
-            model.addAttribute("personas", listaPersona); 
-            int cantidadDePersonas = personaDao.numeroDePaginas();
-            double divisionEntreTotal = cantidadDePersonas / 10;
-            int residuo = cantidadDePersonas % 10;
-            int cantidadPagina = (int) Math.round(divisionEntreTotal);
-            List<Integer> cantidadPaginas = new ArrayList<Integer>();
-            if(residuo != 0)
-            {
-            	cantidadPagina += 1;
-            	for(int i = 1; i < cantidadPagina + 1; i++)
-            	{
-            		cantidadPaginas.add(i);
-            	}
-            }
-            model.addAttribute("candidadDePaginas", cantidadPaginas);
-            
-            return "dashboard";
+	    model.addAttribute("persona", new Persona());
+	    List<Persona> listaPersona = new ArrayList<>();
+	    PersonaDao personaDao = new PersonaDao();
+	    listaPersona = personaDao.pagSeleccionar(0, 10);
+	    model.addAttribute("personas", listaPersona); 
+	    int cantidadDePersonas = personaDao.numeroDePaginas();
+	    double divisionEntreTotal = cantidadDePersonas / 10;
+	    int residuo = cantidadDePersonas % 10;
+	    int cantidadPagina = (int) Math.round(divisionEntreTotal);
+	    List<Integer> cantidadPaginas = new ArrayList<Integer>();
+	    if(residuo != 0)
+	    {
+	    	cantidadPagina += 1;
+	    	for(int i = 1; i < cantidadPagina + 1; i++)
+	    	{
+	    		cantidadPaginas.add(i);
+	    	}
+	    }
+	    model.addAttribute("candidadDePaginas", cantidadPaginas);
+	    
+	    return "dashboard";
 	}
 	
 	@RequestMapping(value="/insertarPersona")
 	public String storeData(Model model)
 	{	
-            model.addAttribute("persona", new Persona());
-            return "nueva_persona";
+	    model.addAttribute("persona", new Persona());
+	    return "nueva_persona";
 	}
 	
 	@RequestMapping(value="/insertarPersona", method=RequestMethod.POST)
 	public String storeData(@ModelAttribute Persona persona, Model model)
 	{
-            PersonaDao personaDao = new PersonaDao();
-            int id = personaDao.insertar(persona);
-            persona.setId(id);
-            listaPersona = personaDao.seleccionar();
-            model.addAttribute("personas", listaPersona); 
-            return "redirect:/dashboard";
+	    PersonaDao personaDao = new PersonaDao();
+	    int id = personaDao.insertar(persona);
+	    persona.setId(id);
+	    listaPersona = personaDao.pagSeleccionar(0, 10);
+	    model.addAttribute("personas", listaPersona); 
+	    return "redirect:/dashboard";
 	}
 	
 	@RequestMapping(value="/paginacion")
@@ -98,9 +99,9 @@ public class AppController
 	{
 		logger.info(webRequest.getParameter("id"));
 		List<Persona> listaPersona = new ArrayList<>();
-        PersonaDao personaDao = new PersonaDao();
-        listaPersona = personaDao.pagSeleccionar(Integer.parseInt(webRequest.getParameter("indice")), 10);
-        modelo.addAttribute("personas", listaPersona); 
-        return listaPersona;
+		PersonaDao personaDao = new PersonaDao();
+		listaPersona = personaDao.pagSeleccionar(Integer.parseInt(webRequest.getParameter("indice")), 10);
+		modelo.addAttribute("personas", listaPersona); 
+		return listaPersona;
 	}
   }
